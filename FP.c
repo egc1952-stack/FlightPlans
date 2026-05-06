77f2077cd26236eec52a36192c00b7f0263c9573
Eric Cutler <ericcutler@comcast.net>
2026-05-06 12:11:30 -0700

    fp 1.0.05 tweaks

--- FP.c
+++ FP.c
@@ -52,7 +52,7 @@ double greatCircleDistance(double lat1, double lon1, double lat2, double lon2) {
     double a = sin(dlat/2)*sin(dlat/2) + cos(lat1*M_PI/180.0)*cos(lat2*M_PI/180.0)*sin(dlon/2)*sin(dlon/2);
     double c = 2 * atan2(sqrt(a), sqrt(1-a));
     return R * c;
-}
+ }
 
 // Check if forcedStart is an END in any constraint
 int isForcedStartAnEnd(int forcedStart, Constraint* constraints, int numConstraints) {
